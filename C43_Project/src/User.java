7fdec473532bdfec2a34e1972a187bb80c2044ee
skandy031 <skandan.vecham@hotmail.com>
2022-07-23 21:50:31 -0400

    Added partial queries for making users

---
+++ C43_Project/src/User.java
@@ -0,0 +1,16 @@
+import java.sql.*;
+import java.util.Scanner;
+public class User {
+  public static void processInfo(Connection con,int sin, String password, String firstName, String lastName,  String occupation,
+                                 String dob){
+    try{
+      String query = "INSERT INTO USERS VALUES(%s, '%s', %s, %s, %s)".format(String.valueOf(sin),password,firstName,lastName,
+                                                                                 occupation, dob);
+      Statement stmt = con.createStatement();
+      stmt.executeUpdate(query);
+    }catch(Exception e){
+      System.err.println("Exception: " + e.getMessage());
+    }
+
+  }
+}
