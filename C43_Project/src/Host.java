441578c6ad068a75668fbcd20cc39e9b07e90181
skandy031 <skandan.vecham@hotmail.com>
2022-07-27 18:12:24 -0400

    progress on create listing

--- C43_Project/src/Host.java
+++ C43_Project/src/Host.java
@@ -1,53 +1,36 @@
 import java.sql.*;
+import java.util.Hashtable;
 import java.util.Scanner;
 
 public class Host {
-
-    static Connection con = null;
-
-    public static void createAccount(Connection connection) {
-        con = connection;
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Username (Enter SIN Number):");
-        int username = scan.nextInt();
-        System.out.println("Password:");
-        String password = scan.next();
-        System.out.println("First name:");
-        String firstname = scan.next();
-        System.out.println("Last name:");
-        String lastname = scan.next();
-        System.out.println("Occupation:");
-        String occupation = scan.next();
-        System.out.println("Date of birth (YYYY-MM-DD):");
-        String dob = scan.next();
-        scan.close();
-//        User.processInfo(con, username, password, firstname, lastname, occupation, dob);
-        try {
-            PreparedStatement s = con.prepareStatement("insert into users values (?,?,?,?,?,?)");
-            s.setInt(1, username);
-            s.setString(2, password);
-            s.setString(3, firstname);
-            s.setString(4, lastname);
-            s.setString(5, occupation);
-            s.setString(6, dob);
-            int status = s.executeUpdate();
-            if (status == 1){
-                System.out.println("Successfully created.");
-            } else{
-                System.out.println("Not able to create account.");
-            }
-        } catch (Exception e){
-            System.out.println(e);
-        }
-        //add this user to the database
-        //write prompts depending on success/fail
-    }
-      public static void handleHostLogin(Connection con) {
+    public static void handleHostLogin(Connection con)  {
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter username:");
-        String username = scan.next();
+        Integer username = scan.nextInt();
         System.out.println("Enter password:");
         String password = scan.next();
+        try{
+            //Query checks Users database
+            PreparedStatement query1 = con.prepareStatement("SELECT sin,password FROM Users WHERE sin = ? AND password = ?");
+            query1.setInt(1, username);
+            query1.setString(2, password);
+            ResultSet rs1 = query1.executeQuery();
+
+            //Query checks Hosts database
+            PreparedStatement query2 = con.prepareStatement("SELECT * FROM Hosts WHERE hostID = ?");
+            query2.setInt(1, username);
+            ResultSet rs2 = query2.executeQuery();
+
+            if(rs1.next() && rs2.next()){
+                System.out.println("Proper");
+                handleHost(con, username);
+            }
+            else{
+                System.out.println("Incorrect Credentials");
+            }
+        }catch(Exception e) {
+            System.out.println(e);
+        }
         //check to see if this matches in database
         //if it does not match
         //print out statement
@@ -56,13 +39,52 @@ public class Host {
         //send to handleHost(user)
         scan.close();
     }
+    public static void listingType(){}
+    public static void createListing(Connection con, Integer username){
+        Hashtable<Integer, String> listingTypes  = new Hashtable<Integer, String>(){{put(1,"Full house");
+            put(2, "Apartment");
+            put(3, "Room");}};
+
+        try {
+            System.out.println("Listing Type:");
+            System.out.println("  (1) Full house");
+            System.out.println("  (2) Apartment");
+            System.out.println("  (3) Room");
+            Scanner scan = new Scanner(System.in);
+            Integer opt = scan.nextInt();
+            System.out.println("Longitude:");
+            Double lon = scan.nextDouble();
+            System.out.println("Latitude:");
+            Double lat = scan.nextDouble();
+            System.out.println("Price");
+            Double price = scan.nextDouble();
+
+            // Insert into Database
+            String query = "INSERT INTO Listing (hostID,listingType,longitude,latitude,price) VALUES (?,?,?,?,?)";
+            PreparedStatement query1 = con.prepareStatement(query);
+            query1.setInt(1, username);
+            query1.setString(2, listingTypes.get(opt));
+            query1.setDouble(3, lon);
+            query1.setDouble(4, lat);
+            query1.setDouble(5, price);
+            query1.executeUpdate();
+        }catch(Exception e){
+            System.out.println(e);
+        }
 
 
-    public void handleHost(String username) {
+
+
+    }
+    public static void handleHost(Connection con, Integer username) {
         //print out all the options
         System.out.println("(1) Create listing");
         System.out.println("(2) View listings");
-        System.out.println("(3) Delete listing");
+        System.out.println("(3) View Bookings");
+        System.out.println("(4) Change Availability");
+        System.out.println("(5) Delete listing");
+        createListing(con,username);
+
 
 
     }
